b59f514e078072db7c578a61153eb05b323dd86f
Vipin <vipinvijayan23@gmail.com>
2021-06-08 23:32:51 -0400

    Chocolate Feat Problem

---
+++ src/solve/problem/ChocolateFeast.java
@@ -0,0 +1,25 @@
+package solve.problem;
+
+public class ChocolateFeast {
+
+	public static void main(String[] args) {
+		int c = chocolateFeast(15, 3, 2);
+		Common.printInt(c);
+	}
+
+	public static int chocolateFeast(int total_money, int cost, int free_wrapper) {
+
+		int wrappers = total_money / cost;
+		int total_chocolates = wrappers;
+
+		while (wrappers >= free_wrapper) {
+			int new_wrappers = wrappers / free_wrapper;
+			int remaining_wrappers = wrappers % free_wrapper;
+			total_chocolates += new_wrappers;
+			wrappers = new_wrappers + remaining_wrappers;
+		}
+
+		return total_chocolates;
+	}
+
+}
---
+++ src/solve/problem/Common.java
@@ -0,0 +1,17 @@
+package solve.problem;
+
+public class Common {
+	
+	static void printString(String txt) {
+		System.out.println(txt);
+	}
+	
+	static void printInt(int number) {
+		System.out.println(String.valueOf(number));
+	}
+	
+	static void printDoublle(int number) {
+		System.out.println(String.valueOf(number));
+	}
+
+}
