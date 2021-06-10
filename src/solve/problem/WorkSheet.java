263bc3a8769701d83f2848d9d9b1cf0c572a1a4f
Vipin <vipinvijayan23@gmail.com>
2021-06-09 22:02:37 -0400

    Workbook code formattiing

--- src/solve/problem/WorkSheet.java
+++ src/solve/problem/WorkSheet.java
@@ -6,7 +6,7 @@ import java.util.List;
 // https://www.hackerrank.com/challenges/lisa-workbook/problem?isFullScreen=false
 
 public class WorkSheet {
-	
+
 	public static void main(String[] args) {
 		List<Integer> arr = new ArrayList<Integer>();
 		arr.add(4);
@@ -14,25 +14,25 @@ public class WorkSheet {
 		int c = workbook(2, 3, arr);
 		Common.printInt(c);
 	}
-	
-	 public static int workbook(int chapters, int k, List<Integer> arr) {
-		    
-         int special_indices = 0;
-         int pages = 1;
-         
-         for(int chapter = 0; chapter < chapters; chapter++){
-             int num_problems = arr.get(chapter);
-             for(int problem = 1; problem <= num_problems; problem++){
-                 if(problem == pages){
-                     special_indices++;
-                 }
-                 if( problem % k == 0 || problem == num_problems){
-                     pages++;
-                 }
-             }
-         }
-         
-         return special_indices;
- }
+
+	public static int workbook(int chapters, int k, List<Integer> arr) {
+
+		int special_indices = 0;
+		int pages = 1;
+
+		for (int chapter = 0; chapter < chapters; chapter++) {
+			int num_problems = arr.get(chapter);
+			for (int problem = 1; problem <= num_problems; problem++) {
+				if (problem == pages) {
+					special_indices++;
+				}
+				if (problem % k == 0 || problem == num_problems) {
+					pages++;
+				}
+			}
+		}
+
+		return special_indices;
+	}
 
 }
